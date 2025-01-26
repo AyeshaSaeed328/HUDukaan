0c5227022999a64e2832f9cf082dfa4cd327b9ca
AyeshaSaeed328 <as07519@st.habib.edu.pk>
2025-01-26 16:49:46 +0500

    fix

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -42,7 +42,7 @@ export default function Cart({ open, setOpen }) {
           <footer className="border-t border-gray-200 p-6">
             <div className="flex justify-between text-base font-medium text-gray-900">
               <p>Subtotal</p>
-              <p>PKR {getTotalCartAmount}</p>
+              <p>PKR {getTotalCartAmount()}</p>
             </div>
             <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
             <div className="flex justify-center mt-6">
